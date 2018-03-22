233a4be5a63238980d638bc8469ca0e5807e78d6
mertkanyener <mertkanyener@outlook.com>
2018-03-22 16:17:45 +0300

    euclidean_dist() completed and tested

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 df = pd.read_csv('data.csv', header=None)
@@ -11,8 +12,9 @@ y = df.loc[:, 'label']
 
 def split_data(X, y, test_size=0.3):
 
-    X_shuffled = X.sample(frac=1)
-    y_shuffled = y.sample(frac=1)
+    shuf_idx = np.random.permutation(X.index)
+    X_shuffled = X.reindex(shuf_idx)
+    y_shuffled = y.reindex(shuf_idx)
     length = X.count()[1]
     i = int((1.0 - test_size ) * length)
     X_train = X_shuffled.iloc[:i, :]
@@ -23,6 +25,29 @@ def split_data(X, y, test_size=0.3):
     return X_train, X_test, y_train, y_test
 
 
-X_train, X_test, y_train, y_test = split_data(X, y)
+def euclidean_dist(train, test):
+
+    dist_M = np.sqrt((train - test[:, None])**2)
+    return dist_M
+
+
+
+
+
+M1 = np.array([(1, 3, 5), (0, 8, 2) ,(4, 1, 7), (9, 3, 6)])
+
+M2 = np.array([(0, 5, 2),
+               (4, 3, 7)])
+
+M_res = euclidean_dist(M1, M2)
+print(M1)
+print(M2)
+print(M_res)
+
+print(M_res[0][0])
+
+print("min value: ", np.min(M_res[0,:]))
+#X_train, X_test, y_train, y_test = split_data(X, y)
+
 
 
--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import pandas as pd
+import numpy as np
 
 df = pd.read_csv('data.csv', header=None)
 df.columns = df.loc[0, :]
 df = df.iloc[1:, :]
 
-X1 = df.loc[:, ('hoursOfStudy')]
-X2 = df.loc[:, ('height', 'midtermGrade')]
 
-X1_S = X1.sample(frac=1)
-X2_S = X2.sample(frac=1)
+M1 = np.arange(6).reshape(2,3)
+M2 = np.arange(12).reshape(4,3)
+
+Z = np.sqrt((M2 - M1[:, None])**2)
 
 
-#print(X1_S)
-#print(X2_S)
+print("M1: ", M1)
+print("---------")
+print("M2: ", M2)
+print("Z: ", Z)
 
-y = df.loc[:, 'label']
+
+
